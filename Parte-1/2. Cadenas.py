93f116f8ca24303705a8f889aa2924050ed4b04e
Joaquin <joacoHRC@outlook.com>
2024-06-24 19:41:45 -0500

    Functions 1

--- Parte-1/2. Cadenas.py
+++ Parte-1/2. Cadenas.py
@@ -4,22 +4,7 @@ def invertCad(cad):
         newCad = i + newCad
     return newCad
 
-def vocales(cad):
-    count = 0
-    for i in cad:
-        if i == "a":
-            count += 1
-        elif i == "e":
-            count += 1
-        elif i == "i":
-            count += 1
-        elif i == "o":
-            count += 1
-        elif i == "u":
-            count += 1
-    return count
-
-
 if __name__ == '__main__':
-    print(invertCad("hola"))
-    print(vocales("holacamarera"))
+    cadena = input("Ingrese una cadena: ")
+    print("Cadena invertida:")
+    print(invertCad(cadena))
