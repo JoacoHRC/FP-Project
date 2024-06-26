81b5d4051dc8ca7e2dfa12c6e1f3aac214388851
SL7u777 <isaac.martel.b@uni.pe>
2024-06-26 04:13:33 -0500

    Add files via upload
    Cree un correo de pruebas usalo si gustas y en cuanto a lo de enviar el email salia un poco de error si puedes arreglarla a buena hora y go comentariozzzzzzzzzz

---
+++ AplicativoWeb/Modulos/Academica/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
+
+def formularioContacto(request):
+    return render (request ,"formularioContacto.html")
+def contactar (request):
+    if request.method=="POST":
+        asunto = request.POST["txtAsunto"]
+        mensaje= request.POST["txtMensaje"]+"/ Email" + request.POST["txtEmail"]
+        email_desde= settings.EMAIL_HOST_USER
+        email_para = ["isaac.martel.b@uni.pe"]
+        send_mail(asunto,mensaje,email_desde,email_para,fail_silently=False)
+        return render(request , "contactoExitoso.html")
+    return render(request , "formularioContacto.html")
+
